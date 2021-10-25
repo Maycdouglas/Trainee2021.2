77292e4677f62fe3760b1dc7707e606e66501300
Igor Abrantes <igor@igorabrantes.com>
2021-10-25 19:51:35 -0300

    Carrossel e últimos produtos

---
+++ mycaopetshop/public/js/home.js
@@ -0,0 +1,20 @@
+let backToTopButton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
