c7350016ad6ba9853ea3be7d29bd3fbfaab21643
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-11-29 10:48:26 -0300

    adiciona o nome do banco de dados

--- mycaopetshop/config.php
+++ mycaopetshop/config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'nome_do_banco',
+        'name' => 'mycaopetshop',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
