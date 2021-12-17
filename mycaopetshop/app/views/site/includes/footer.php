0d92f3e081209a1f0db3af1932507389eb39b28a
IsaCristine <bell2011jf@gmail.com>
2021-12-17 15:51:49 -0300

    Coloca Navbar e Footer em arquivos separados

---
+++ mycaopetshop/app/views/site/includes/footer.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../public/css/navbarft.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://kit.fontawesome.com/c11bde093d.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+
+
+
+
+<body>
+
+    <footer class="py-4 mt-4 bg-blue">
+        <div class="container">
+            <div class="row border-bottom justify-content-center align-items-center m-0 mb-4">
+                <ul class="nav justify-content-center mb-4">
+                    <li class="nav-item"><a href="/home" class="nav-link px-2 links-footer">Início</a></li>
+                    <li class="nav-item"><a href="/quemsomos" class="nav-link px-2 links-footer">Sobre Nós</a></li>
+                    <li class="nav-item"><a href="/produtos" class="nav-link px-2 links-footer">Nossos Produtos</a></li>
+                    <li class="nav-item"><a href="/contato" class="nav-link px-2 links-footer">Fale Conosco</a></li>
+                </ul>
+            </div>
+
+
+            <div class="row justify-content-center align-items-center m-0 mb-2">
+                <div class="col-md-8 col-12">
+                    <p class="text-center my-2 texto-footer">Sed ut perspiciatis unde omnis iste natus error sit
+                        voluptatem
+                        accusantium doloremque laudantium, totam rem
+                        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                        explicabo.
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                        consequuntur.</p>
+                </div>
+            </div>
+
+
+            <div class="row justify-content-center align-items-center m-0 mb-2">
+                <div class="col-md-8 col-12">
+                    <div class="row justify-content-center align-items-center m-0 mb-2">
+
+                        <ul class="nav justify-content-center m-0 p-0">
+                            <li class="nav-item"><a href="mailto: maycaopetshop@gmail.com" class="nav-link px-2 links-footer"><i class="fas fa-envelope redes-sociais"></i></a></li>
+                            <li class="nav-item"><a href="https://www.instagram.com" class="nav-link px-2 links-footer"><i class="fab fa-instagram redes-sociais"></i></a></li>
+                            <li class="nav-item"><a href="https://www.twitter.com" class="nav-link px-2 links-footer"><i class="fab fa-twitter redes-sociais"></i></a></li>
+                            <li class="nav-item"><a href="https://web.whatsapp.com" class="nav-link px-2 links-footer"><i class="fab fa-whatsapp redes-sociais"></i></a></li>
+                            <li class="nav-item"><a href="https://www.facebook.com" class="nav-link px-2 links-footer"><i class="fab fa-facebook redes-sociais"></i></a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row justify-content-center align-items-center m-0">
+                <p class="text-center text-muted m-0 p-0 mt-2">© 2021 Code Jr</p>
+            </div>
+
+
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
