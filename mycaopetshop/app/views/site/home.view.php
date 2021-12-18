39c564562ed8a8ffd3f48c40013f19153aa05da8
IsaCristine <bell2011jf@gmail.com>
2021-12-18 15:32:10 -0300

    Inclui Navbar e Footer na Home

--- mycaopetshop/app/views/site/home.view.php
+++ mycaopetshop/app/views/site/home.view.php
@@ -22,30 +22,8 @@
         </svg>
     </button>
 
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark shadow bg-blue">
-            <div class="container">
-
-                <a href="/home" class="d-flex align-items-center mb-0 text-decoration-none">
-                    <img class="logo-navbar p-0 m-0" src="../../public/assets/Logo escrita.png">
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/home">Home</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/produtos">Produtos</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/contato">Contato</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/quemsomos">Quem Somos</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/login">Login</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php include 'app/views/includes/navbar.php';?>
+    
     <section class="container-principal">
         <div class="carousel slide mb-0" data-bs-ride="carousel" id="carousel">
             <div class="carousel-indicators">
@@ -129,55 +107,7 @@
     </main>
 
 
-    <footer class="py-4 mt-4 bg-blue">
-        <div class="container">
-            <div class="row border-bottom justify-content-center align-items-center m-0 mb-4">
-                <ul class="nav justify-content-center mb-4">
-                    <li class="nav-item"><a href="/home" class="nav-link px-2 links-footer">Início</a></li>
-                    <li class="nav-item"><a href="/quemsomos" class="nav-link px-2 links-footer">Sobre Nós</a></li>
-                    <li class="nav-item"><a href="/produtos" class="nav-link px-2 links-footer">Nossos Produtos</a></li>
-                    <li class="nav-item"><a href="/contato" class="nav-link px-2 links-footer">Fale Conosco</a></li>
-                </ul>
-            </div>
-
-
-            <div class="row justify-content-center align-items-center m-0 mb-2">
-                <div class="col-md-8 col-12">
-                    <p class="text-center my-2 texto-footer">Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem
-                        accusantium doloremque laudantium, totam rem
-                        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                        explicabo.
-                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                        consequuntur.</p>
-                </div>
-            </div>
-
-
-            <div class="row justify-content-center align-items-center m-0 mb-2">
-                <div class="col-md-8 col-12">
-                    <div class="row justify-content-center align-items-center m-0 mb-2">
-
-                        <ul class="nav justify-content-center m-0 p-0">
-                            <li class="nav-item"><a href="mailto: maycaopetshop@gmail.com" class="nav-link px-2 links-footer"><i class="fas fa-envelope redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="https://www.instagram.com" class="nav-link px-2 links-footer"><i class="fab fa-instagram redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="https://www.twitter.com" class="nav-link px-2 links-footer"><i class="fab fa-twitter redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="https://web.whatsapp.com" class="nav-link px-2 links-footer"><i class="fab fa-whatsapp redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="https://www.facebook.com" class="nav-link px-2 links-footer"><i class="fab fa-facebook redes-sociais"></i></a></li>
-                        </ul>
-
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row justify-content-center align-items-center m-0">
-                <p class="text-center text-muted m-0 p-0 mt-2">© 2021 Code Jr</p>
-            </div>
-
-
-        </div>
-    </footer>
+    <?php include 'app/views/includes/footer.php';?>
 
     <script crossorigin="anonymous" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script defer src="../../public/js/home.js"></script>
