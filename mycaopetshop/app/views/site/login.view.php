687698e1ad7b93020f633a88f1ddbdc3d23dbc63
IsaCristine <bell2011jf@gmail.com>
2021-12-17 23:10:54 -0300

    Inclui Navbar na página de Login

--- mycaopetshop/app/views/site/login.view.php
+++ mycaopetshop/app/views/site/login.view.php
@@ -24,30 +24,7 @@
 
 <body class="corpo-login justify-content-center align-content-center">
 
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark shadow bg-blue">
-            <div class="container">
-
-                <a href="/home" class="d-flex align-items-center mb-0 text-decoration-none">
-                    <img class="logo-navbar p-0 m-0" src="../../public/assets/Logo escrita.png">
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/home">Home</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/produtos">Produtos</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/contato">Contato</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/quemsomos">Quem Somos</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="/login">Login</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php include 'app/views/site/includes/navbar.php'; ?>
 
     <!-- <div class="container justify-content-center align-content-center">
         <div class="row justify-content-center align-content-center pt-0 m-0">
