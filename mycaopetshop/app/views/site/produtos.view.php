5efcc20b79aafd4e7da356566fc5ce2adfa7b1c7
IsaCristine <bell2011jf@gmail.com>
2021-12-17 23:14:41 -0300

    Inclui Navbar e Footer na página de Produtos

--- mycaopetshop/app/views/site/produtos.view.php
+++ mycaopetshop/app/views/site/produtos.view.php
@@ -5,44 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/css/produtos.css">
     <link rel="stylesheet" href="../../public/css/navbarft.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
-        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>MyCão Pet Shop - Produtos</title>
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark shadow bg-blue">
-            <div class="container">
 
-                <a href="#" class="d-flex align-items-center mb-0 text-decoration-none">
-                    <img class="logo-navbar p-0 m-0" src="../../public/assets/Logo escrita.png">
-                </a>
+    <?php include 'app/views/site/includes/navbar.php'; ?>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="home.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="produtos.view.php">Produtos</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="contato.html">Contato</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="quemsomos.html">Quem Somos</a></li>
-                        <li class="nav-item"><a class="nav-item nav-link links-navbar" href="login.html">Login</a></li>
-                    </ul>
-                </div>
-
-            </div>
-        </nav>
-    </header>
     <main class="container">
         <section>
             <div class="principal text-center mt-4">
@@ -65,7 +39,7 @@
                         <a href="view-produto.html" class="btn btn-details">Ver Detalhes</a>
                     </div>
                 </div><!-- card1 -->
-                <?php foreach ($produtos as $produto) :?>
+                <?php foreach ($produtos as $produto) : ?>
                     <div class="card cards" style="width: 18rem;">
                         <a href="view-produto.html"><img src="<?= $produto->foto ?>" class="card-img-top" alt="..."></a>
                         <div class="card-body">
@@ -79,86 +53,31 @@
                         </div>
                     </div><!-- card1 -->
                 <?php endforeach; ?>
-            <div class="pagination">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                      <li class="page-item disabled">
-                        <a class="page-link">Previous</a>
-                      </li>
-                      <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">Next</a>
-                      </li>
-                    </ul>
-                  </nav>
-            </div>
+                <div class="pagination">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item disabled">
+                                <a class="page-link">Previous</a>
+                            </li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">4</a></li>
+                            <li class="page-item"><a class="page-link" href="#">5</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
         </section>
     </main>
 
 
-    <footer class="py-4 mt-4 bg-blue">
-        <div class="container">
-            <div class="row border-bottom justify-content-center align-items-center m-0 mb-4">
-                <ul class="nav justify-content-center mb-4">
-                    <li class="nav-item"><a href="home.html" class="nav-link px-2 links-footer">Início</a></li>
-                    <li class="nav-item"><a href="quemsomos.html" class="nav-link px-2 links-footer">Sobre Nós</a></li>
-                    <li class="nav-item"><a href="produtos.view.php" class="nav-link px-2 links-footer">Nossos Produtos</a></li>
-                    <li class="nav-item"><a href="contato.html" class="nav-link px-2 links-footer">Fale Conosco</a></li>
-                </ul>
-            </div>
+    <?php include 'app/views/site/includes/footer.php'; ?>
 
 
-            <div class="row justify-content-center align-items-center m-0 mb-2">
-                <div class="col-md-8 col-12">
-                    <p class="text-center my-2 texto-footer">Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem
-                        accusantium doloremque laudantium, totam rem
-                        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                        explicabo.
-                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                        consequuntur.</p>
-                </div>
-            </div>
-
-
-            <div class="row justify-content-center align-items-center m-0 mb-2">
-                <div class="col-md-8 col-12">
-                    <div class="row justify-content-center align-items-center m-0 mb-2">
-
-                        <ul class="nav justify-content-center m-0 p-0">
-                            <li class="nav-item"><a href="#" class="nav-link px-2 links-footer"><i
-                                        class="fas fa-envelope redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="#" class="nav-link px-2 links-footer"><i
-                                        class="fab fa-instagram redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="#" class="nav-link px-2 links-footer"><i
-                                        class="fab fa-twitter redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="#" class="nav-link px-2 links-footer"><i
-                                        class="fab fa-whatsapp redes-sociais"></i></a></li>
-                            <li class="nav-item"><a href="#" class="nav-link px-2 links-footer"><i
-                                        class="fab fa-facebook redes-sociais"></i></a></li>
-                        </ul>
-
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row justify-content-center align-items-center m-0">
-                <p class="text-center text-muted m-0 p-0 mt-2">© 2021 Code Jr</p>
-            </div>
-
-
-        </div>
-    </footer>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
