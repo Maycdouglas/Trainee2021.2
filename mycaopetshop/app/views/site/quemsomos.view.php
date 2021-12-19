5a1f7c6b1699c4edf5b3eb854bf13531fbe9877e
lucasvarino <lucasvarino@gmail.com>
2021-12-18 22:42:58 -0300

    Adicionando a fonte na pagina de quem somos

--- mycaopetshop/app/views/site/quemsomos.view.php
+++ mycaopetshop/app/views/site/quemsomos.view.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="../../public/css/quemsomos.css">
     <link rel="stylesheet" href="../../public/css/navbarft.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
     <title>Quem Somos</title>
 </head>
 
